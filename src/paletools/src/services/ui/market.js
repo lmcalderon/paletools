20762d3b09fbbc56800b3049ca53bb20583c927e
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2022-09-28 14:49:20 -0300

    bugfixes

--- src/paletools/src/services/ui/market.js
+++ src/paletools/src/services/ui/market.js
@@ -1,11 +1,29 @@
 import { addMarketSearchPreRender } from "../../core-overrides/UTMarketSearchResultsViewControllerOverrides";
 import localize from "../../localization";
 import delay from "../../utils/delay";
+import { select } from "../../utils/dom";
 import { notifySuccess } from "../../utils/notifications";
 import { tryBuyItem } from "../market";
 import { navigateBack } from "./navigation";
 
+let scrollToBuyNow = false;
+
+const UTMarketSearchFiltersViewController_viewDidAppear = UTMarketSearchFiltersViewController.prototype.viewDidAppear;
+UTMarketSearchFiltersViewController.prototype.viewDidAppear = async function viewDidAppear() {
+    UTMarketSearchFiltersViewController_viewDidAppear.call(this);
+
+    await delay(10);
+    const inputs = selectAll(".ut-numeric-input-spinner-control");
+    if(inputs.length > 3){
+        inputs[0].scrollIntoView();
+    }
+
+    scrollToBuyNow = true;
+}
+
 export function enableMarketSnipe(shouldSnipeFunc, onBack = null, onSnipeSuccess = null, onSnipeFailure = null) {
+
+
     addMarketSearchPreRender((items, controller) => {
         if (!shouldSnipeFunc()) return true;
 
@@ -14,9 +32,7 @@ export function enableMarketSnipe(shouldSnipeFunc, onBack = null, onSnipeSuccess
                 onBack();
             }
             navigateBack(controller);
-            delay(10).then(() => {
-                document.querySelectorAll(".ut-numeric-input-spinner-control")[3].scrollIntoView()
-            });
+            scrollToBuyNow = true;
         }
 
         setTimeout(() => {
